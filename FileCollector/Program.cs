a2fd67f73fea1ed3ecfb842c1564b8295ed5aa20
Lorenzo Di Mauro <lorenzo.dimauro@we-plus.eu>
2025-05-23 10:53:27 +0200

    First commit

---
+++ FileCollector/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace FileCollector
+{
+    class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+            appBuilder.Services
+                .AddLogging();
+
+            // register root component and selector
+            appBuilder.RootComponents.Add<App>("app");
+
+            var app = appBuilder.Build();
+
+            // customize window
+            app.MainWindow
+                .SetIconFile("favicon.ico")
+                .SetTitle("File Collector");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
+
+        }
+    }
+}
